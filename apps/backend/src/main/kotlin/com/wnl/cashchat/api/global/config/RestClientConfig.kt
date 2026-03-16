760d4e8f739b8736c89b62faee2cad4180bb5ab4
Jiwoong Choi <seedplan005@gmail.com>
2026-03-17 01:26:53 +0900

    [CC-148] Back-End Project Set-Up
    * Project 의존성 설정
    * 환경별 설정 파일 분리 (base/dev/prod)
    * Domain에서 공통적으로 사용하는 구성 요소 설정
    - JPA Auditing + BaseEntity
    - RestClient

---
+++ apps/backend/src/main/kotlin/com/wnl/cashchat/api/global/config/RestClientConfig.kt
@@ -0,0 +1,13 @@
+package com.wnl.cashchat.api.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+@Configuration
+class RestClientConfig {
+
+    @Bean
+    fun restClient(): RestClient = RestClient.create()
+
+}
